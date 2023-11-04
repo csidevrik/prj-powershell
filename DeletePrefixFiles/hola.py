87e6453ca340a42307ccac49ce4e5684c9eb4265
carlos sigua <carlos.sigua@gmail.com>
2023-11-04 18:16:03 -0500

    cambios en python

--- DeletePrefixFiles/hola.py
+++ DeletePrefixFiles/hola.py
@@ -1,6 +1,20 @@
-import flet as ft
+import os
+import fnmatch
 
-def main (page: ft.Page):
-    page.add(ft.Text("Hola, fletex!"))
+def get_files_pdf(path):
+    files_pdf = []
+    for (root, dirs, files) in os.walk(path):
+        for filename in files:
+            if fnmatch.fnmatch(filename, '*.pdf'):
+                file_complete = os.path.join(root,filename)
+                files_pdf.append(file_complete)
+                print(file_complete)
+    
+    return files_pdf
 
-ft.app(main)
+def main():
+    directorio = '/home/adminos/OneDriveP/2A-JOB02-EMOVEP/2023/CONTRATOS/RE-EP-EMOVEP-2023-02/FACTURAS/SEP2'
+    listdbs = get_files_pdf(directorio)
+
+if __name__ == "__main__":
+    main()
