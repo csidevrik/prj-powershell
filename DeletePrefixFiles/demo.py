3467707ca67061f94386ffbae6dd0bc24f59128b
Carlos Sigua <carlos.sigua@gmail.com>
2024-02-25 16:35:55 -0500

    subir

--- DeletePrefixFiles/demo.py
+++ DeletePrefixFiles/demo.py
@@ -3,49 +3,61 @@ import flet as ft
 name = "Draggable VerticalDivider"
 
 async def main(page: ft.Page):
-    page.window_width = 1920
-    page.window_height = 1080
+    page.window_width = 960
+    page.window_height = 540
     page.title = "facturet"
     page.bgcolor = "#263238"
 
     async def move_vertical_divider(e: ft.DragUpdateEvent):
-        if (e.delta_x > 0 and c.width < 800) or (e.delta_x < 0 and c.width > 400):
-            c.width += e.delta_x
-        await c.update_async()
+        if (e.delta_x > 0 and cleft.width < 360) or (e.delta_x < 0 and cleft.width > 200):
+            cleft.width += e.delta_x
+        await cleft.update_async()
 
     async def show_draggable_cursor(e: ft.HoverEvent):
         e.control.mouse_cursor = ft.MouseCursor.RESIZE_LEFT_RIGHT
         await e.control.update_async()
+    # /////////////////////////////////////////////////////////////
+    
+    inputSearch = ft.TextField(
+        hint_text="SEARCH", 
+        text_align=ft.TextAlign.CENTER,
+        border=ft.InputBorder.OUTLINE,
+        filled=True,
+        bgcolor="#f6f8fa",
+        helper_text="Ingresa aqui la herramienta que deseas usar",
+        # helper_style=ft.ShadowBlurStyle.SOLID,
+    )
 
-    c = ft.Container(
-        bgcolor=ft.colors.ORANGE_300,
+
+    cleft = ft.Container(inputSearch,
+        bgcolor="#026ec1",
         alignment=ft.alignment.center,
         width=300,
         # expand=1,
     )
+    cright = ft.Container(
+        bgcolor= "#263238",
+        alignment=ft.alignment.center,
+        expand=1,
+    )
 
     fila= ft.Row(
+        spacing=0,
+        width=960,
+        height=540,
         controls=[
-            c,
+            cleft,
             ft.GestureDetector(
                 content=ft.VerticalDivider(),
                 drag_interval=10,
                 on_pan_update=move_vertical_divider,
                 on_hover=show_draggable_cursor,
             ),
-            ft.Container(
-                bgcolor= "#263238",
-                alignment=ft.alignment.center,
-                expand=1,
-            ),
+            cright,
         ],
-        spacing=0,
-        width=1920,
-        height=1080,
+        
     )
     
     await page.add_async(fila)
     pass
-
-
 ft.app(target=main)
